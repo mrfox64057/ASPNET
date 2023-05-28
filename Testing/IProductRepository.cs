5de9eacf1346c3dbd6bf28dcffaacdb40419a270
Adam Jankowski <ghost17814@gmail.com>
2023-05-28 17:35:55 -0400

    bleh

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -16,5 +16,7 @@ namespace Testing
         public IEnumerable<Category> GetCategories();
 
         public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
     }
 }
