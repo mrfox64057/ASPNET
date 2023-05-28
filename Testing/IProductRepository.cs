b56025ea954cda39b98edf7f9464e61d81a34af3
Adam Jankowski <ghost17814@gmail.com>
2023-05-28 17:26:46 -0400

    copy paste

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,5 +10,11 @@ namespace Testing
         public Product GetProduct(int id);
 
         public void UpdateProduct (Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
     }
 }
